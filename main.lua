a05df5d0e6951bbc40f7a5a76666dddbf41cd85e
Nikita Rakcheev <nrakcheev4@gmail.com>
2025-03-28 13:52:32 +0300

    L4-0: Init commit

---
+++ main.lua
@@ -0,0 +1,115 @@
+function love.load()
+    anim8 = require 'anim8/anim8' -- animatons
+    sti = require 'Simple-Tiled-Implementation/sti' -- map generation
+    camera = require 'hump/camera' -- camera chasing player
+    wf = require 'windfield/windfield'
+
+    world = wf.newWorld(0, 0)
+
+    cam = camera()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    gameMap = sti('resources/maps/map.lua')
+
+
+    player = {}
+    player.collider = world:newBSGRectangleCollider(400, 250, 50, 100, 10)
+    player.collider:setFixedRotation(true)
+    player.x = 400
+    player.y = 200
+    player.speed = 300
+    --player.sprite = love.graphics.newImage('resources/sprites/parrot.png')
+    player.spriteSheet = love.graphics.newImage('resources/sprites/player-sheet.png')
+    player.grid = anim8.newGrid(12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight());
+
+    player.animations = {}
+    player.animations.down = anim8.newAnimation(player.grid('1-4', 1), 0.2)
+    player.animations.up = anim8.newAnimation(player.grid('1-4', 4), 0.2)
+    player.animations.left = anim8.newAnimation(player.grid('1-4', 2), 0.2)
+    player.animations.right = anim8.newAnimation(player.grid('1-4', 3), 0.2)
+
+    player.anim = player.animations.left
+
+    --background = love.graphics.newImage('resources/sprites/background.png')
+    walls = {}
+    if gameMap.layers['Walls'] then
+        for i, obj in pairs(gameMap.layers['Walls'].objects) do
+            local wall = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+            wall:setType('static')
+            table.insert(walls, wall)
+        end
+    end
+
+    sounds = {}
+    sounds.relaxBackgroundMusic = love.audio.newSource('resources/sounds/relaxed-scene.mp3', 'stream')
+    sounds.relaxBackgroundMusic:setLooping(true)
+    sounds.relaxBackgroundMusic:play()
+end
+
+function love.update(dt)
+    local isMoving = false
+    local vx = 0
+    local vy = 0
+
+    if love.keyboard.isDown("right") then
+        vx = player.speed
+        player.anim = player.animations.right
+        isMoving = true
+    end
+    if love.keyboard.isDown("left") then
+        vx = -player.speed
+        player.anim = player.animations.left
+        isMoving = true
+    end
+    if love.keyboard.isDown("up") then
+        vy = -player.speed
+        player.anim = player.animations.up
+        isMoving = true
+    end
+    if love.keyboard.isDown("down") then
+        vy = player.speed
+        player.anim = player.animations.down
+        isMoving = true
+    end
+
+    if isMoving == false then
+        player.anim:gotoFrame(2)
+    end
+    player.collider:setLinearVelocity(vx, vy)
+
+    world:update(dt)
+    player.x = player.collider:getX()
+    player.y = player.collider:getY()
+
+    player.anim:update(dt)
+
+    cam:lookAt(player.x, player.y)
+
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+
+    -- Left border
+    if cam.x < w / 2 then cam.x = w / 2 end
+    -- Top border
+    if cam.y < h / 2 then cam.y = h / 2 end
+
+    local mapW = gameMap.width * gameMap.tilewidth
+    local mapH = gameMap.height * gameMap.tileheight
+
+    -- Right border
+    if cam.x > (mapW - w / 2) then cam.x = mapW - w / 2 end
+    -- Bottom border
+    if cam.y > (mapH - h / 2) then cam.y = mapH - h / 2 end
+end
+
+function love.draw()
+    --love.graphics.circle("fill", player.x, player.y, 100)
+    --love.graphics.draw(player.sprite, player.x, player.y)
+    --love.graphics.draw(background, 0, 0)
+    cam:attach()
+        -- gameMap:draw()
+        gameMap:drawLayer(gameMap.layers['Ground'])
+    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 6, nil, 6, 9)
+    gameMap:drawLayer(gameMap.layers['Trees'])
+    --world:draw() -- if you wanna see colliders in the world
+    cam:detach()
+end
